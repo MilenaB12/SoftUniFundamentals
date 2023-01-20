3396b57dfcc6c549888d081a70f2c182c7ce4c98
Milena Boyadzhieva <123003398+MilenaB12@users.noreply.github.com>
2023-01-20 14:37:40 +0200

    Add code

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/01. Student Information/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string name = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+double grade = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade:f2}");
